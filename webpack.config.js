41de68e35801d7166e216a4be727145547519368
zhoululu <1442802810@qq.com>
2020-04-18 16:50:21 +0800

    添加webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html'
+    })
+  ],
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true
+  }
+}
